0a34acc22aba8f1ac299dfff78426f3afa6d9d65
Hans Nielsen <hans@stackallocated.com>
2019-07-27 16:58:59 -0700

    Factor out some functions

--- shim.go
+++ shim.go
@@ -20,40 +20,60 @@ type LogEntry struct {
 	Body      string   `json:"body"`
 }
 
-func main() {
-	// read stdin
-	body, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		log.Fatalf("couldn't read stdin: %v", err)
-	}
-
-	// build JSON
-	entry := LogEntry{
-		Arguments: os.Args[1:],
-		Body:      string(body),
-		Time:      time.Now().UTC().Format(time.RFC3339),
-	}
+func PopulateEntry(e *LogEntry) error {
+	// set the time
+	e.Time = time.Now().UTC().Format(time.RFC3339)
 
 	// get calling user ID and name
 	u, err := user.Current()
 	if err == nil {
-		entry.UserID = u.Uid
-		entry.Username = u.Username
+		e.UserID = u.Uid
+		e.Username = u.Username
 	} else {
 		// just fill in the user ID
-		entry.UserID = fmt.Sprintf("%d", os.Getuid())
+		e.UserID = fmt.Sprintf("%d", os.Getuid())
+	}
+
+	// just use the full arguments list minus the program name
+	e.Arguments = os.Args[1:]
+
+	// read stdin
+	body, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		return fmt.Errorf("couldn't read stdin: %v", err)
+	}
+	e.Body = string(body)
+
+	return nil
+}
+
+func EmitLog() error {
+	// build JSON
+	entry := LogEntry{}
+	err := PopulateEntry(&entry)
+	if err != nil {
+		return err
 	}
 
 	// write out the JSON object on a line by itself
 	f, err := os.OpenFile(LogFilePath, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0644)
 	if err != nil {
-		log.Fatalf("couldn't open log file: %v", err)
+		return fmt.Errorf("couldn't open log file: %v", err)
 	}
 	defer f.Close()
 
 	j := json.NewEncoder(f)
 	err = j.Encode(entry)
 	if err != nil {
-		log.Fatalf("couldn't encode JSON: %v", err)
+		return fmt.Errorf("couldn't encode JSON: %v", err)
+	}
+
+	return nil
+}
+
+func main() {
+	err := EmitLog()
+	if err != nil {
+		log.Fatal(err)
 	}
 }
