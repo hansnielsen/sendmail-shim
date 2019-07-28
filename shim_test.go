9b4a7b9c0bc2a64d9473e645dfd4e487f031ceaa
Hans Nielsen <hans@stackallocated.com>
2019-07-27 18:02:49 -0700

    Add JSON encoder tests

--- shim_test.go
+++ shim_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path"
@@ -57,5 +59,41 @@ func TestOpenLogFile(t *testing.T) {
 	expected := "foo\nbar\n"
 	if string(contents) != expected {
 		t.Errorf("expected %q, got %q", expected, contents)
+	}
+}
+
+type ErrorWriter struct{}
+
+func (_ ErrorWriter) Write(_ []byte) (int, error) {
+	return 0, fmt.Errorf("oh no")
+}
+
+func TestEncodeJSON(t *testing.T) {
+	// test JSON encoding errors
+	lerr := EncodeJSON(ErrorWriter{}, LogEntry{})
+	if lerr == nil {
+		t.Fatal("expected an error")
+	}
+	if lerr.Tag != "json-encoding" {
+		t.Fatalf("unexpected tag %q", lerr.Tag)
+	}
+
+	// test expected encoding
+	e := LogEntry{
+		Time:      "2009-11-10T23:00:00Z",
+		UserID:    "123",
+		Username:  "foo",
+		Arguments: []string{"yay", "asdf"},
+		Body:      "stuff",
+	}
+	expected := "{\"time\":\"2009-11-10T23:00:00Z\",\"uid\":\"123\",\"username\":\"foo\",\"arguments\":[\"yay\",\"asdf\"],\"body\":\"stuff\"}\n"
+	b := bytes.Buffer{}
+	lerr = EncodeJSON(&b, e)
+	if lerr != nil {
+		t.Fatal(lerr.Err)
+	}
+	actual := b.String()
+	if expected != actual {
+		t.Fatalf("expected %q, got %q", expected, actual)
 	}
 }
